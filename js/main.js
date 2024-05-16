900e70d5e0f11a0a927143e8bc8efe36bc758f85
Daisy <kiennguyen12.com@gmail.com>
2024-05-16 20:50:19 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,72 @@
+$(document).ready(function () {
+  $(".carousel__main").slick({
+    dots: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    appendDots: $(".slick-slider-dots"),
+    nextArrow:
+      "<i class='bi bi-caret-right a-left control-c prev slick-next'></i>",
+    prevArrow:
+      "<i class='bi bi-caret-left a-right control-c next slick-prev'></i>",
+    responsive: [
+      {
+        breakpoint: 1000,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+        },
+      },
+      {
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 680,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+});
+function handleHeader() {
+  var prevScrollpos = window.pageYOffset;
+  window.onscroll = function () {
+    var header = document.querySelector("header");
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+      header.style.top = "0px";
+    } else {
+      if (currentScrollPos > header.offsetHeight) {
+        header.style.top = "-100px";
+      }
+    }
+    prevScrollpos = currentScrollPos;
+  };
+}
+handleHeader();
+function progressbar() {
+  progress = document.querySelector(".progressbar");
+  window.addEventListener("scroll", () => {
+    let Y = window.scrollY;
+    let percent =
+      (scrollY / (document.body.offsetHeight - window.innerHeight)) * 100;
+    progress.style.width = `${percent}%`;
+  });
+}
+progressbar();
+navigattion = () => {
+  const btn = document.querySelector(".btnmenu");
+  const nav = document.querySelector(".nav");
+  btn.addEventListener("click", function () {
+    this.classList.toggle("active");
+    nav.classList.toggle("active");
+    document.body.classList.toggle("active");
+  });
+};
+navigattion();
